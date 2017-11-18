41f6559bff247bd044416e0f9e9557206e15ff9a
Brian J. Dillard <armchairdj@gmail.com>
2017-11-18 12:32:05 -0800

    - Corral system tests.

--- test/system/songs_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class SongsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit songs_url
-  #
-  #   assert_selector "h1", text: "Song"
-  # end
-end
