c6c30409cddc5e911a1dd99602eabd9884bb4f71
Brian J. Dillard <armchairdj@gmail.com>
2017-11-30 11:27:49 -0800

    - Complete transition to columnar desktop layout.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
 
   # GET /
   def index
+    # flash.now[:error] = "This is an error message."
+    # flash.now[:notice] = "This is a notice message."
+
     @homepage = true
   end
 
