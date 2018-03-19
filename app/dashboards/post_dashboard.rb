635c36de8e96d8404711ea8c04799b5f1029e64e
Brian J. Dillard <armchairdj@gmail.com>
2018-03-19 08:19:58 -0700

    - Toward admin.

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -1,27 +1,16 @@
 require "administrate/base_dashboard"
 
 class PostDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    postable: Field::Polymorphic,
-    id: Field::Number,
-    title: Field::String,
-    body: Field::Text,
+    postable:     Field::Polymorphic,
+    id:           Field::Number,
+    title:        Field::String,
+    body:         Field::Text,
     published_at: Field::DateTime,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    created_at:   Field::DateTime,
+    updated_at:   Field::DateTime,
   }.freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
     :postable,
     :id,
@@ -29,8 +18,6 @@ class PostDashboard < Administrate::BaseDashboard
     :body,
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
     :postable,
     :id,
@@ -41,9 +28,6 @@ class PostDashboard < Administrate::BaseDashboard
     :updated_at,
   ].freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :postable,
     :title,
@@ -51,10 +35,7 @@ class PostDashboard < Administrate::BaseDashboard
     :published_at,
   ].freeze
 
-  # Overwrite this method to customize how posts are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(post)
-  #   "Post ##{post.id}"
-  # end
+  def display_resource(post)
+    "Post ##{post.id}"
+  end
 end
