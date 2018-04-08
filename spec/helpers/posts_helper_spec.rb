4d65a25f4a2a3bef2dc9bde9854e3e5ecf73a6c4
Brian J. Dillard <armchairdj@gmail.com>
2018-04-08 10:33:49 -0700

    - Change description of standalone posts.

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PostsHelper, type: :helper do
-  let(:standalone_post      ) { create(:standalone_post, title: "Standalone") }
+  let(:standalone_post      ) { create(:standalone_post, title: 'Standalone') }
   let(:hounds_of_love_review) { create(:hounds_of_love_album_review) }
 
-  describe "#link_to_post" do
-    it "uses title for standalone" do
+  describe '#link_to_post' do
+    it 'uses title for standalone' do
       actual = helper.link_to_post(standalone_post)
 
       expect(actual).to have_tag('a[href^="/posts/"]',
-        text:  "Standalone",
+        text:  'Standalone',
         count: 1
       )
     end
 
-    it "by default uses work title for review" do
+    it 'by default uses work title for review' do
       actual = helper.link_to_post(hounds_of_love_review)
 
       expect(actual).to have_tag('a[href^="/posts/"]',
-        text:  "Hounds of Love",
+        text:  'Hounds of Love',
         count: 1
       )
     end
 
-    it "can use creator name and work title for review" do
+    it 'can use creator name and work title for review' do
       actual = helper.link_to_post(hounds_of_love_review, full: true)
 
       expect(actual).to have_tag('a[href^="/posts/"]',
@@ -33,8 +33,8 @@ RSpec.describe PostsHelper, type: :helper do
     end
   end
 
-  describe "#post_type" do
-    specify{ expect(helper.post_type(standalone_post      )).to eq("standalone"  ) }
-    specify{ expect(helper.post_type(hounds_of_love_review)).to eq("album review") }
+  describe '#post_type' do
+    specify{ expect(helper.post_type(standalone_post      )).to eq('standalone post') }
+    specify{ expect(helper.post_type(hounds_of_love_review)).to eq('album review'   ) }
   end
 end
