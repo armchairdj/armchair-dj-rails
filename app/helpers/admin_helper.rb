4a948824166202a61776c8d30b032b7b195b4a85
Brian J. Dillard <armchairdj@gmail.com>
2018-04-13 10:25:36 -0700

    - Admin views.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -23,7 +23,7 @@ module AdminHelper
   #############################################################################
 
   def admin_create_link(model)
-    svg   = semantic_svg_image("open_iconic/plus.svg", title: "plus sign", desc: "addition icon")
+    svg   = semantic_svg_image("open_iconic/plus.svg", title: "plus sign", desc: "create icon")
     path  = new_polymorphic_path([:admin, model])
     title = "new #{model.model_name.singular}"
 
@@ -31,7 +31,7 @@ module AdminHelper
   end
 
   def admin_destroy_link(instance)
-    svg   = semantic_svg_image("open_iconic/x.svg", title: "x", desc: "delete icon")
+    svg   = semantic_svg_image("open_iconic/x.svg", title: "x", desc: "destroy icon")
     path  = polymorphic_path([:admin, instance])
     title = "destroy #{instance.model_name.singular}"
 
@@ -41,15 +41,24 @@ module AdminHelper
   end
 
   def admin_view_link(instance)
-    svg   = semantic_svg_image("open_iconic/eye.svg", title: "eyeball", desc: "eyeball icon")
-    path  = polymorphic_path([:admin, instance])
+    svg   = semantic_svg_image("open_iconic/eye.svg", title: "eyeball", desc: "view icon")
+    path  ||= polymorphic_path([:admin, instance])
     title = "view #{instance.model_name.singular}"
 
-    link_to(svg, path, title: title, class: "admin show")
+    link_to(svg, path, title: title, class: "admin view")
+  end
+
+  def admin_public_view_link(instance, path = nil)
+    path  ||= polymorphic_path(instance)
+
+    svg   = semantic_svg_image("open_iconic/eye.svg", title: "globe", desc: "public view icon")
+    title = "view #{instance.model_name.singular} on site"
+
+    link_to(svg, path, title: title, class: "admin public-view")
   end
 
   def admin_update_link(instance)
-    svg   = semantic_svg_image("open_iconic/pencil.svg", title: "pencil", desc: "pencil icon")
+    svg   = semantic_svg_image("open_iconic/pencil.svg", title: "pencil", desc: "update icon")
     path  = edit_polymorphic_path([:admin, instance])
     title = "edit #{instance.model_name.singular}"
 
