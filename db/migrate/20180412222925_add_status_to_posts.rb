6fb50d5a02dc9bad77d9eedb8dbff611e01f96f7
Brian J. Dillard <armchairdj@gmail.com>
2018-04-12 17:42:34 -0700

    - Remove dev-only migration of existing data.

--- db/migrate/20180412222925_add_status_to_posts.rb
+++ db/migrate/20180412222925_add_status_to_posts.rb
@@ -2,8 +2,6 @@ class AddStatusToPosts < ActiveRecord::Migration[5.1]
   def up
     add_column :posts, :status, :integer, default: 0, null: false
     add_index :posts, :status
-
-    Post.update_all(status: :published)
   end
 
   def down
