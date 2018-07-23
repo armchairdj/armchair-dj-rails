d991e256b21af9841eb2b34e060b12055b7d033b
Brian J. Dillard <armchairdj@gmail.com>
2018-07-23 15:52:39 -0700

    - A tiny bit more cleanup.

--- lib/templates/rspec/policy_spec.rb
+++ lib/templates/rspec/policy_spec.rb
@@ -7,37 +7,37 @@ RSpec.describe <%= class_name %>Policy do
   context "without user" do
     let(:user) { nil }
 
-    pending "works"
+    pending "works without user"
   end
 
   describe "as member" do
     let(:user) { create(:member) }
 
-    pending "works"
+    pending "works with member"
   end
 
   describe "as writer" do
     let(:user) { create(:writer) }
 
-    pending "works"
+    pending "works with writer"
   end
 
   describe "as editor" do
     let(:user) { create(:editor) }
 
-    pending "works"
+    pending "works with editor"
   end
 
   describe "as admin" do
     let(:user) { create(:admin) }
 
-    pending "works"
+    pending "works with admin"
   end
 
   context "as root" do
     let(:user) { create(:root) }
 
-    pending "works"
+    pending "works with root"
   end
 
   describe "scope" do
@@ -45,7 +45,7 @@ RSpec.describe <%= class_name %>Policy do
 
     before(:each) do
       allow( model_class).to receive(:all).and_call_original
-      expect(model_class).to receive(:all)#.with(user)
+      expect(model_class).to receive(:all)
     end
 
     describe "with user" do
